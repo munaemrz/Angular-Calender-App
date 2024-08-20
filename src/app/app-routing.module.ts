7392bc47d11d1dc265d54e2fe88e148b31ea3b36
Munaem Rudab <munaem.rudab@live.com>
2024-08-20 14:36:22 +0500

    feat angular calender app with drag and drop appointments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CalendarComponent } from './calender/calender.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'calendar', pathMatch: 'full' },
-  { path: 'calendar', component: CalendarComponent },
+  { 
+    path: 'calendar', 
+    loadChildren: () => import('./calender/calender.module').then(m => m.CalendarModule)
+  },
 ];
 
 @NgModule({
